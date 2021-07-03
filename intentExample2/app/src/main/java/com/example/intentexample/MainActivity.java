b2b1589f9f512395a035e33e3725fd32975c5702
cacaocoffee <75840431+cacaocoffee@users.noreply.github.com>
2021-07-03 19:32:13 +0900

    imageView,Toast 사용
    이미지뷰와 토스트 사용 , 정렬을 어떻게 하는지 Linearlayout 을 통해 사용

--- intentExample2/app/src/main/java/com/example/intentexample/MainActivity.java
+++ intentExample2/app/src/main/java/com/example/intentexample/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_move;
     private EditText et_test;
     private String str;
+    private ImageView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
         et_test=findViewById(R.id.et_test);
         btn_move=findViewById(R.id.btn_move);
-
+        test=(ImageView) findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"명석",Toast.LENGTH_SHORT).show();
+            }
+        });
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
